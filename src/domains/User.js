da0cbf0d6989879a52cb9bbbab454e86f297aad4
ChrisPeng <pengchi1244@gmail.com>
2018-09-09 11:32:58 +0800

    Add eth address to user table

--- src/domains/User.js
+++ src/domains/User.js
@@ -4,9 +4,11 @@ class User {
     id: number;
     balance: number;
     inviteCode: string;
-    constructor(id: number, balance: number, inviteCode: string = '') {
+    address: string;
+    constructor(id: number, balance: number, inviteCode: string = '', address: string = '') {
         this.id = id;
         this.balance = balance;
+        this.address = address;
         this.inviteCode = inviteCode;
     }
 }
